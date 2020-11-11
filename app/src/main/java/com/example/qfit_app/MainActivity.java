39e05930546b877e11035ac71d9fa3f20e9f8728
Uriel <umihura@itba.edu.ar>
2020-11-10 23:23:45 -0300

    comienzo de home y search views
    falta la lista de las rutinas en cada una, me parece que hay que armar un fragment que sea una rutina y despues hacer una lista de esos

--- app/src/main/java/com/example/qfit_app/MainActivity.java
+++ app/src/main/java/com/example/qfit_app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.qfit_app;
 
 import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
     }
 
 }
